fe0cee1aba37f0df135ae6ec8a3778622cc15537
Keerthi-Vibisan-S <83780331+Keerthi-Vibisan-S@users.noreply.github.com>
2021-10-16 19:04:40 +0530

    Day 6

--- Supports/JS/index.js
+++ Supports/JS/index.js
@@ -11,3 +11,31 @@ function toogleNav()
         ele.classList.add('hide');
     }
 }
+
+
+function Show()
+{
+    let element = document.querySelector('#hidden-menu');
+
+    let iconz = document.querySelector("#drop-iconz");
+
+    if(iconz.classList.contains('fa-arrow-up'))
+    {
+        hide();
+    }
+
+    else{
+        iconz.classList.remove('fa-arrow-down');
+        iconz.classList.add('fa-arrow-up');
+        element.style.display = "block";
+    }
+}
+
+function hide()
+{
+    let element = document.querySelector('#hidden-menu');
+    element.style.display = "none";
+    let iconz = document.querySelector("#drop-iconz");
+    iconz.classList.add('fa-arrow-down');
+    iconz.classList.remove('fa-arrow-up');
+}
